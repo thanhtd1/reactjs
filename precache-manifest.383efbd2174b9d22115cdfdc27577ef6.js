a37f45dd27eb3fd50b7d7b20d8dae70708e46600
thanhtd1 <thanhtd1to9@gmail.com>
2019-09-12 18:01:33 +0700

    Updates

---
+++ precache-manifest.383efbd2174b9d22115cdfdc27577ef6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce00157395741ebd75554d0d8edf4ac0",
+    "url": "/reactjs/index.html"
+  },
+  {
+    "revision": "26b535a8284b4d52fea5",
+    "url": "/reactjs/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "95ea4bf960ff57988162",
+    "url": "/reactjs/static/js/2.25ee4f03.chunk.js"
+  },
+  {
+    "revision": "26b535a8284b4d52fea5",
+    "url": "/reactjs/static/js/main.ba5ae9f7.chunk.js"
+  },
+  {
+    "revision": "544eb17fefb0afab81a6",
+    "url": "/reactjs/static/js/runtime~main.c9fad93d.js"
+  }
+]);
